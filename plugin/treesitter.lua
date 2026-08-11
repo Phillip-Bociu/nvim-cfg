1abed3c387b5dc17e8748ae750c8f5f14abb5769
Phillip Bociu <phillipbociu01@gmail.com>
2026-08-11 10:16:01 +0300

    fixed some error when opening certain file formats

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -6,27 +6,15 @@ vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
     end
 end })
 
-vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
-    once = true,
+vim.pack.add({
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
+    'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+})
+
+require('nvim-treesitter').setup()
+
+vim.api.nvim_create_autocmd('FileType', {
     callback = function()
-        vim.pack.add({
-            'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
-            { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
-        })
-        require("nvim-treesitter.configs").setup({
-            sync_install = false,
-            ignore_install = { "javascript" },
-            modules = {},
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            auto_install = true,
-            ensure_installed = {
-                "c",
-                "cpp",
-                "cmake",
-            },
-        })
+        pcall(vim.treesitter.start)
     end,
 })
